ba6a5a17c6586f4a523bded6fc0bf53bda37004a
Ahmad Muhammad <kowaslaba2015@gmail.com>
2022-09-09 03:45:44 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
      *
      * Return: Always 1.
      * Desription: "C program that prints exactly and that piece of art is useful"
-     */
+ */
 int main(void)
 {
   write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
