c5e74cc3b80b6943c685a07a8e1e3340f41686cc
Ahmad Muhammad <kowaslaba2015@gmail.com>
2022-09-09 00:07:19 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,8 @@
 #include <unistd.h>
 
 /**
- * main - Prints "and that piece of art is useful" -Dora Korpar, 2015-10-19", follwed by a new line, to the standard error.
+ * main - Prints "and that piece of art is useful" -Dora Korpar, 2015-10-19",
+ * follwed by a new line, to the standard error.
  *
  * Return: Always 1.
  */
