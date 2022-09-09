92574de9c42b10d71d8ddede7b3f804f70306281
Ahmad Muhammad <kowaslaba2015@gmail.com>
2022-09-09 03:52:25 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,10 +4,8 @@
 /**
      * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
      * followed by a new line to standard error.
-     *
      * Return: Always 1.
-     * Desription: "C program that prints exactly and that piece of art is useful"
- */
+     */
 int main(void)
 {
   write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
