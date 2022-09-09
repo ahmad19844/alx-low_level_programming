aeea8c36ef5a005e3fea61fcaab4d87cd48c82c8
Ahmad Muhammad <kowaslaba2015@gmail.com>
2022-09-09 04:50:08 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,13 @@
  * File: 101-quote.c
  * Auth: Brennan D Baraban
  */
-#include <stdio.h>
+
  #include <unistd.h>
 
 /*
  * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
  *                followed by a new line, to standard error.
- *
+ 
  * Return: Always 1.
  */
 int main(void)
