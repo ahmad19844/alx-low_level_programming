34887f4c345f067fb8cbcd06b225958f0a262ef5
Ahmad Muhammad <kowaslaba2015@gmail.com>
2022-09-08 21:22:53 -0700

    UNIX is basically a simple operating system

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+/* 
+ * File:101-quate.c
+ * Auth: Brennan D Baraban 
+ */
+#include <unistd.h>
+
+/**
+ * main - Prints "and that piece of art  is useful" -and that piece of art is useful" - Dora Korpar, 2015-10-19"
+ *
+ * Return always 1.
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\n"-Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
+
