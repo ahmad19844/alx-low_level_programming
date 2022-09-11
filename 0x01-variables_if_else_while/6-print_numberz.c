59ea8452b3b557e0610c33352a5e0a9c426cfcc0
Ahmad Muhammad <kowaslaba2015@gmail.com>
2022-09-11 04:11:56 -0700

    Print number of base 10 from 0

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - program that prints all single digit numbers of base 10 starting from 0
+ *
+ * Decription: using main function
+ * this program will prints "all single digit numbers of base 10 starting from 0
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		putchar((a % 10) + '0');
+	putchar('\n');
+
+	return (0);
+}
