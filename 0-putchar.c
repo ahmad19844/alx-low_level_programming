ae700eff82e0addb3ec416bc3b0b7d472d56a22e
Ahmad Muhammad <kowaslaba2015@gmail.com>
2022-09-13 04:55:41 -0700

    _putchar

---
+++ 0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - a program that prints _putchar, followed by a new line.
+ *
+ * Description: using main function
+ * This Program will prints " _putchar, followed by a new line.
+ * Return: 0
+ */
+int main (void)
+{
+	char c[] = "Ahmad;
+	int a;
+
+	for (a = 0; a < 9; a++)
+	{
+		_putchar(c[a]);
+	}
+	_putchar('\n');
+	return (0);
+}
+
