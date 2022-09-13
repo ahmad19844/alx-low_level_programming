5a01bd6c5f3ed9ad4e6c5a2cdf378853bae35d9e
Ahmad Muhammad <kowaslaba2015@gmail.com>
2022-09-13 16:16:40 -0700

    print putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,7 @@
 #include "main.h"
 /**
-<<<<<<< HEAD
  * main - print putchar
  * Rerurn: always 0
-=======
- * main - prints putchar
- * Return: always 0
->>>>>>> b3d7769f57f47669ea7acfab7ad75a4161289e97
  */
 int main(void)
 {
@@ -15,19 +10,12 @@ int main(void)
 	_putchar('u');
 	_putchar('t');
 	_putchar('c');
-<<<<<<< HEAD
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
 	
-	_putchar('\n');
+	_putchar("\n");
 
-=======
-        _putchar('h');
-        _putchar('a');
-        _putchar('r');
-	putchar("\n");
->>>>>>> b3d7769f57f47669ea7acfab7ad75a4161289e97
 	return (0);
 }
 
