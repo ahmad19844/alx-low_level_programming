656a0a31a3f580cd2b20794c908c990c24402c0f
Ahmad Muhammad <kowaslaba2015@gmail.com>
2022-09-13 05:16:41 -0700

    program that prints _putchar, followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - program that prints _putchar, followed by a new line
+ *
+ * Description: using main function
+ * This program prints " _putchar
+ * Return: 0
+ */
+int main(void)
+{
+	char c[] = "AHmad";
+	in a;
+
+	for (a = 0; a < 9; a++)
+	{
+		_putchar(c[a]);
+	}
+	_putchar('\n');
+	return (0);
+}
+
