089dd2706ebe27201ed8d73d3e6ec9c1cf96fba8
Ahmad Muhammad <kowaslaba2015@gmail.com>
2022-09-13 06:36:55 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,11 @@
-#include "holberton.h"
+#include "putchar.h"
 /**
  * main - Entry point
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char *sh = "Holberton";
+	char *sh = "_putchar";
 	
 	while (*sh)
 	{
