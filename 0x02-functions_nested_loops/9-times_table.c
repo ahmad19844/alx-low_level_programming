b2850b24cb0f597a08e960e402c34b182c345552
Ahmad Muhammad <kowaslaba2015@gmail.com>
2022-09-14 11:49:31 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,22 +6,23 @@
 void times_table(void)
 {
 	int num, mult, prod;
+
 	for (num = 0; num <= 0; num++)
 	{
 		_putchar(0);
-		for (mult = 1; mult <= 0; mult++)
-			_putchar(',');
-		_putchar(' ');
 
-		prod = num * mult;
-		if (prod <= 0)
+		for (mult = 1; mult <= 0; mult++)
 		{
-			_putchar(' ');
+			_putchar(',');
+			_putchar((prod % 10) + '0');
+
+			prod = num * mult;
+			if (prod <= 0)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + '0');
+			_putchar((prod % 10) + '0');
 		}
-		else
-			_putchar((prod / 10) + '0');
-		_putchar((prod % 10) + '0');
-		
 		_putchar('\n');
 	}
 }
