468f76af94867cf79e4f9f2bb7a28f9aaa374eae
Ahmad Muhammad <kowaslaba2015@gmail.com>
2022-09-19 07:56:13 -0700

    reverse string 2

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,6 +12,7 @@ void rev_string(char *s)
 
 	char r[1000];
 	i = j = 0;
+
 	while (s[i] != '\0')
 	{
 		r[i] = s[i];
@@ -25,4 +26,6 @@ void rev_string(char *s)
 		j++;
 	}
 	s[j++] = '\0';}
+	}
+}
 
