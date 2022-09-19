81382a2c3008fadbe75ad4bd12b6f1719f3a3acd
Ahmad Muhammad <kowaslaba2015@gmail.com>
2022-09-19 06:02:43 -0700

    swapping

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,6 +4,7 @@
  * Swap_int - Switches Value of First input with the value of the second input.
  * @a: First number to swap.
  * @b: Second number to swap
+ *
  * Description: Using main function
  * This function prints "Swapped number
  * Return: 0
