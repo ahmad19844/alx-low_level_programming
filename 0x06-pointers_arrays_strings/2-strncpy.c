ac8e61d0909254e429f97e257f2db930d371032e
Ahmad Muhammad <kowaslaba2015@gmail.com>
2022-09-21 14:58:37 -0700

    string copy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,7 +4,6 @@
  * _strncpy - cpoies n bytes of src to the dest string
  * @dest: string to copy to
  * @src: string being copied* @n: largest number of bytes to copy
- *
  * Return: address of destt
  */
 char *_strncpy(char *dest, char *src, int n)
@@ -13,7 +12,6 @@ char *_strncpy(char *dest, char *src, int n)
 	int i;
 
 	i = 0;
-
 	while (i < n && *(src + i))
 	{
 		*(dest + i) = *(src + i);
