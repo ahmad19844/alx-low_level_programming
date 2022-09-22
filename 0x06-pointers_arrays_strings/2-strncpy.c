36766721022f677fe61fb3e28053abfb977f3b5a
Ahmad Muhammad <kowaslaba2015@gmail.com>
2022-09-21 20:29:01 -0700

    string copy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,8 +3,9 @@
 /**
  * _strncpy - cpoies n bytes of src to the dest string
  * @dest: string to copy to 
- * @src: string being copie
+ * @src: string being copied
  * @n: largest number of bytes to copy
+ *
  * Return: address of destt
  */
 char *_strncpy(char *dest, char *src, int n)
