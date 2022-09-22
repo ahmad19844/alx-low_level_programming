59a66f297605bdbdb522404a93c2dbd6bbf37806
Ahmad Muhammad <kowaslaba2015@gmail.com>
2022-09-21 22:25:48 -0700

    string copy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,23 +1,27 @@
 #include "main.h"
 
 /**
- * _strncpy - copies n bytes of src to the dest string
- *            of byte from string scr to dest
- * @dest: string to copy to 
+ * _strncpy - cpoies n bytes of src to the dest string
+ * @dest: string to copy to
  * @src: string being copied
  * @n: largest number of bytes to copy
+ *
  * Return: address of dest
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int index = 0, src_len = 0;
+	int i;
 
-	while(src[index++])
-		src_len++;
-	for(index = 0; src[index] && index < n; index++)
-		dest[index] = src[index];
-	for(index = src_len; index < n; index++)
-		dest[index] = '\0';
-	
+	i = 0;
+	while (i < n && *(src + i))
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	while (i < n)
+	{
+		*(dest + i) = '\0';
+		i++;
+	}
 	return (dest);
 }
