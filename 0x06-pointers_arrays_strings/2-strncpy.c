765c2cddc45a451dd2a74beebb0cdc48f17f9065
Ahmad Muhammad <kowaslaba2015@gmail.com>
2022-09-21 21:10:37 -0700

    string copy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,13 +10,16 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int index = 0, dest_len = 0;
+	int index = 0, src_len = 0;
 
-	while (dest[index++])
-		dest_len++;
+	while (src[index++])
+		src_len++;
 
 	for (index = 0; src[index] && index < n; index++)
-		dest[dest_len++] = src[index];
+		dest[index] = src[index];
+
+	for (index = src_len, index < n, index++)
+		dest[index] = '\0';
 	
 	return(dest);
 }
