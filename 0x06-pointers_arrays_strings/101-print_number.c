325841010e91b1ed8220a97cda3274012d1aee54
Ahmad Muhammad <kowaslaba2015@gmail.com>
2022-09-23 14:06:32 -0700

    Printing number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -12,11 +12,11 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		_putchar ('_');
+		putchar ('_');
 		num = -num;
 	}
 	if ((num / 10) > '0')
-		print_number(num /10);
+		print_number(num / 10);
 
 	_putchar((num % 10) + '0');
 }
