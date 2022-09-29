cced5dbb01f7ecbe225b3f497cf1cfc4d73ac568
Ahmad Muhammad <kowaslaba2015@gmail.com>
2022-09-28 19:00:53 -0700

    wild char

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -53,7 +53,7 @@ int wildcmp(char *s1, char *s2)
 		return (wildcmp(s1, s2 + 1));
 	else if (*s2 == '*')
 	{
-		p = wildcmp(s1, s2 + 1);
+		p = wildcard(s1, s2 + 1);
 		if (p == 0)
 			return (0);
 		return (wildcmp(p + 1, s2 + 2));
@@ -63,4 +63,4 @@ int wildcmp(char *s1, char *s2)
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
 	return (wildcmp(s1 + 1, s2 + 1));
-}i
+}
