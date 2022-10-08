1ddc5ee2d3da9e904bf0a8e3c96b6612c067e481
Ahmad Muhammad <kowaslaba2015@gmail.com>
2022-10-07 20:42:38 -0700

    multiply two numbers

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int num1, num2, mul;
+	printf("Enter two numbers:");
+	scanf("%d %d",&num1,&num2);
+	mul = num1 * num2;
+	printf("Product of two numbers: %d",mul);
+	return  (0);
+}
