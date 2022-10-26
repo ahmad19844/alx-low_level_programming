ebaf9fe9a2429fe54443e5570d8a989e8e69c2a4
Ahmad Muhammad <kowaslaba2015@gmail.com>
2022-10-25 20:58:00 -0700

    Reverse the list:

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,30 +1,30 @@
 #include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
+  * reverse_listint - ...
+  * @head: ...
+  *
+  * Return: ...
+  */
+listint_t *reverse_listint(listint_t **head)
 {
-    listint_t *head;
+	listint_t *prev = NULL, *next = NULL;
 
-    head = NULL;
-    add_nodeint_end(&head, 0);
-    add_nodeint_end(&head, 1);
-    add_nodeint_end(&head, 2);
-    add_nodeint_end(&head, 3);
-    add_nodeint_end(&head, 4);
-    add_nodeint_end(&head, 98);
-    add_nodeint_end(&head, 402);
-    add_nodeint_end(&head, 1024);
-    print_listint(head);
-    reverse_listint(&head)
-    print_listint(head);    
-    free_listint2(&head);
-    return (0);
+	if (head)
+	{
+		while (*head)
+		{
+			next = *head;
+			*head = (*head)->next;
+			next->next = prev;
+			prev = next;
+		}
+
+		*head = prev;
+		return (*head);
+	}
+
+	return (NULL);
 }
 
