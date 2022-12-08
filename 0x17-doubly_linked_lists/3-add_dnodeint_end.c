059f13843c94e8b3f335a8b8c4a8b839563a8260
Ahmad Muhammad <kowaslaba2015>
2022-12-08 02:14:41 -0800

    add list

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,18 +1,17 @@
 #include <stdlib.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
-  * add_dnodeint - Adds a new node at the beginning
+  * add_dnodeint_end - Adds a new node at the end
   * of a doubly linked list
   * @head: The head of the doubly linked list
   * @n: The number to place in the new node
   *
   * Return: The new head of the doubly linked list
   */
-dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *new_node = NULL;
+	dlistint_t *current = NULL, *new_node = NULL;
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
@@ -21,11 +20,14 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new_node->n = n;
 	if (*head)
 	{
-		new_node->next = *head;
-		new_node->prev = (*head)->prev;
-		(*head)->prev = new_node;
-		*head = new_node;
-		return (*head);
+		current = *head;
+		while (current->next != NULL)
+			current = current->next;
+
+		new_node->next = NULL;
+		new_node->prev = current;
+		current->next = new_node;
+		return (new_node);
 	}
 
 	new_node->next = *head;
