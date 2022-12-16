5f08f5aabc9936009bab3cea943a7e809bccfbe9
Ahmad Muhammad <kowaslaba2015>
2022-12-16 03:19:11 -0800

    Tasko 0x18

---
+++ 0x18-dynamic_libraries/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isalpha - tests whether a character is from the English alphabet
+ * @c: character to test.
+ * Return: 1 if it is a lowercase letter, 0 if it is not a lowercase letter.
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
