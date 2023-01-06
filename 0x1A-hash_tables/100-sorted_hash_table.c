4d03c1fb0324c59d1908e480ad15802c74eca8ab
Ahmad Muhammad <kowaslaba2015@gmail.com>
2023-01-06 13:08:00 -0800

    Hhash table

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -295,8 +295,8 @@ void shash_table_print_rev(const shash_table_t *ht)
 void shash_table_delete(shash_table_t *ht)
 {
 	unsigned long int i = 0;
-
 	shash_node_t *node;
+
 	while (i < ht->size)
 	{
 		while (ht->array[i] != NULL)
